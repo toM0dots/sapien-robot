fe94cdf7ac15eb746d75d1dc7630749e8bb482c9
anthonyjclark <anthonyjclark@gmail.com>
2025-05-13 10:42:31 -0700

    Updated the sampled action demo.

--- experiments/demo-sampled-action.py
+++ experiments/demo-sampled-action.py
@@ -12,24 +12,16 @@ import gymnasium as gym
 from twsim.envs import terrain  # noqa: F401
 from twsim.robots import transwheel  # noqa: F401
 
-env = gym.make(
-    "Terrain-env",
-    # num_envs=1,
-    # obs_mode="state",
-    # control_mode="pd_ee_delta_pose",  # there is also "pd_joint_delta_pos", ...
-    # render_mode="human",
-)
+env = gym.make("Terrain-env")
+env.unwrapped.print_sim_details()  # type: ignore
 
-print("Observation space", env.observation_space)
-print("Action space", env.action_space)
+# Reset with a seed for determinism
+obs, _ = env.reset(seed=0)
 
-obs, _ = env.reset(seed=0)  # reset with a seed for determinism
 done = False
-
 while not done:
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
-    # env.render()  # a display is required to render
 
 env.close()
